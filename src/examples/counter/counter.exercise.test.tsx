a89d1f8a2b7edd8fffe3bbba3aea4bc244768931
Antonios Gkamitsios <angam97@gmail.com>
2023-10-05 19:09:08 +0300

    More updates

--- src/examples/counter/counter.exercise.test.tsx
+++ src/examples/counter/counter.exercise.test.tsx
@@ -1,3 +1,5 @@
+// @vitest-environment happy-dom
+
 import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
@@ -20,9 +22,24 @@ test('it should increment when the "Increment" button is pressed', async () => {
   expect(currentCount).toHaveTextContent('1');
 });
 
-test.todo('it should render the component with an initial count', () => {});
+test('it should render the component with an initial count', () => {
+  const initialCount = 5;
+  render(<Counter initialCount={initialCount} />);
+  const currentCount = screen.getByTestId('current-count');
 
-test.todo(
-  'it should reset the count when the "Reset" button is pressed',
-  async () => {},
-);
+  expect(currentCount).toHaveTextContent(String(initialCount));
+});
+
+test('it should reset the count when the "Reset" button is pressed', async () => {
+  const initialCount = 5;
+  const user = userEvent.setup();
+  render(<Counter initialCount={initialCount} />);
+  
+  const header = screen.getByText(/Gitastrophe™/)
+  const currentCount = screen.getByTestId('current-count');
+  const button = screen.getByRole('button', { name: /reset/i });
+
+  await user.click(button);
+
+  expect(currentCount).toHaveTextContent('0');
+});
--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -10,19 +10,48 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  render(<PackingList />);
+  screen.getByLabelText('New Item Name');
+});
 
-it.todo(
-  'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
-);
+it('has a "Add New Item" button that is disabled when the input is empty', () => {
+  render(<PackingList />);
 
-it.todo(
-  'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
-);
+  const button = screen.getByText('➕ Add New Item');
+  const input = screen.getByLabelText('New Item Name');
 
-it.todo(
-  'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
-);
+  expect(input).toHaveValue('');
+  expect(button).toBeDisabled();
+});
+
+it('enables the "Add New Item" button when there is text in the input field', async () => {
+  const userInput = 'Panagia';
+  const { user } = render(<PackingList />);
+
+  const input = screen.getByLabelText('New Item Name');
+  const button = screen.getByText('➕ Add New Item');
+
+  expect(input).toHaveValue('');
+  expect(button).toBeDisabled();
+
+  await user.type(input, userInput);
+
+  expect(input).toHaveValue(userInput);
+  expect(button).not.toBeDisabled();
+});
+
+it('adds a new item to the unpacked item list when the clicking "Add New Item"', async () => {
+  const userInput = 'Panagia';
+  const { user } = render(<PackingList />);
+
+  const input = screen.getByLabelText('New Item Name');
+  const button = screen.getByText('➕ Add New Item');
+
+  await user.type(input, userInput);
+  await user.click(button);
+  const item = screen.getByLabelText(userInput);
+
+  expect(item).not.toBeChecked();
+});
+
